c3ed55138fdeaf84273ca629142e9d4f3194c102
Dominik Brader <d.brader@findologic.com>
2019-08-23 11:42:22 +0200

    Rename FinSearchUnified to FinSearch

---
+++ src/FinSearch.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1);
+
+namespace FINDOLOGIC\FinSearch;
+
+use Shopware\Core\Framework\Plugin;
+
+class FinSearch extends Plugin
+{
+}
--- src/FinSearchUnified.php
+++
@@ -1,9 +0,0 @@
-<?php declare(strict_types=1);
-
-namespace FINDOLOGIC\FinSearchUnified;
-
-use Shopware\Core\Framework\Plugin;
-
-class FinSearchUnified extends Plugin
-{
-}
