792530ca356441bee75548a9b466fc39c5d6c93b
Huzaifa Mustafa <24492269+zaifastafa@users.noreply.github.com>
2019-11-26 22:09:26 +0500

    SW-328-implement-promotions

---
+++ src/Struct/Promotion.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types=1);
+
+namespace FINDOLOGIC\FinSearch\Struct;
+
+use Shopware\Core\Framework\Struct\Struct;
+
+class Promotion extends Struct
+{
+    /** @var string */
+    private $image;
+
+    /** @var string */
+    private $link;
+
+    public function __construct(string $image, string $link)
+    {
+        $this->image = $image;
+        $this->link = $link;
+    }
+
+    public function getImage(): string
+    {
+        return $this->image;
+    }
+
+    public function getLink(): string
+    {
+        return $this->link;
+    }
+}
