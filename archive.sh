b1fd998a7f082d441435dc2e10fa0c0af753a068
Dominik Brader <d.brader@findologic.com>
2020-05-05 14:40:21 +0200

    Update archive.sh and create separate file for shopware store

--- archive.sh
+++ archive.sh
@@ -45,27 +45,27 @@ cp -rf . /tmp/FinSearch
 # Get into the created directory for running the archive command
 cd /tmp/FinSearch
 
-echo "Running composer install..."
-# Install dependencies
+echo "Installing dependencies with 'composer install' ..."
 composer install --no-dev --optimize-autoloader --prefer-dist
+cd /tmp
+# Run archive command to create the zip in the root directory
+ZIP_FILE="${ROOT_DIR}/FinSearch-${VERSION}.zip"
+zip -r9 ${ZIP_FILE} FinSearch -x FinSearch/phpunit.xml.dist FinSearch/phpcs.xml FinSearch/tests/\* \
+ FinSearch/archive.sh FinSearch/.gitignore FinSearch/.travis.yml FinSearch/.idea/\* FinSearch/.git/\* *.zip \
+ FinSearch/composer.lock
 
+echo "Requiring 'shopware/core' and 'shopware/storefront' for the shopware store..."
+cd /tmp/FinSearch
+# The store needs to know which shopware dependencies are required.
+composer require shopware/core:"^6.1" shopware/storefront:"^6.1"
 cd /tmp
 
 ZIP_FILE_STORE="${ROOT_DIR}/FinSearch-${VERSION}-store.zip"
-ZIP_FILE_MANUAL_INSTALL="${ROOT_DIR}/FinSearch-${VERSION}-manual-install.zip"
-# Run archive command to create the zip in the root directory
 zip -r9 ${ZIP_FILE_STORE} FinSearch -x FinSearch/phpunit.xml.dist FinSearch/phpcs.xml FinSearch/tests/\* \
  FinSearch/archive.sh FinSearch/.gitignore FinSearch/.travis.yml FinSearch/.idea/\* FinSearch/.git/\* *.zip \
  FinSearch/composer.lock FinSearch/README.md FinSearch/vendor/\*
 
-# Remove dependencies that are not required for a normal build.
-composer remove shopware/core shopware/storefront
-
-zip -r9 ${ZIP_FILE_MANUAL_INSTALL} FinSearch -x FinSearch/phpunit.xml.dist FinSearch/phpcs.xml FinSearch/tests/\* \
- FinSearch/archive.sh FinSearch/.gitignore FinSearch/.travis.yml FinSearch/.idea/\* FinSearch/.git/\* *.zip \
- FinSearch/composer.lock
-
-# Delete the directory after script execution
+# Delete the directory after script execution.
 rm -rf '/tmp/FinSearch'
 
 cd ${ROOT_DIR}
@@ -74,9 +74,9 @@ echo 'Restoring work in progress ...'
 
 git checkout ${BRANCH}
 
-# Only apply stash if there are some local changes
+# Only apply stash if there are some local changes.
 if [[ ${STASH} != 'No local changes to save' ]]; then
     git stash pop
 fi
 
-echo -e "${GREEN}Release was successfully created! Location: '${ZIP_FILE}'"
+echo -e "${GREEN}Release was successfully created! Location: '${ZIP_FILE_STORE}'"
