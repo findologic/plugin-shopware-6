c5c6664eb691fa2526407938f48cdb4ee3d0b568
Huzaifa Mustafa <24492269+zaifastafa@users.noreply.github.com>
2020-05-20 20:08:35 +0500

    Sw 416 show staging button (#59) SW-435-improve-config-design-and-validate-shopkey (#64)
    * SW-416-show-staging-button-in-config
    
    * SW-416-show-staging-button-in-config
    
    * Review changes and fixes
    
    * Review changes
    
    * Review changes
    
    * Minor indentation fix
    
    * SW-435-improve-config-design-and-validate-shopkey (#64)
    
    * SW-435-improve-config-design-and-validate-shopkey
    
    * Minor changes
    
    * Changes in config help-text
    
    * Review fix

---
+++ src/Resources/app/administration/src/module/findologic-module/index.js
@@ -0,0 +1,31 @@
+import './extension/sw-plugin';
+import './page/findologic-page';
+import './components/findologic-config';
+
+import enGB from './snippet/en-GB';
+import deDE from './snippet/de-DE';
+
+const { Module } = Shopware;
+
+Module.register('findologic-module', {
+    type: 'plugin',
+    name: 'FinSearch',
+    title: 'findologic.header',
+    description: 'findologic.general.mainMenuDescription',
+    color: '#f7ff0f',
+
+    snippets: {
+        'de-DE': deDE,
+        'en-GB': enGB
+    },
+
+    routes: {
+        index: {
+            component: 'findologic-page',
+            path: 'index',
+            meta: {
+                parentPath: 'sw.settings.index'
+            }
+        }
+    }
+});
