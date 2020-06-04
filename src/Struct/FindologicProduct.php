717d8e5332ed1aa28fdd6a7ff0bd21c7d835d356
tobiasgraml11 <t.graml@findologic.com>
2020-06-04 17:09:01 +0200

    Change code

--- src/Struct/FindologicProduct.php
+++ src/Struct/FindologicProduct.php
@@ -819,11 +819,11 @@ class FindologicProduct extends Struct
 
     private function getSortedImages(): ProductMediaCollection
     {
-        if ($this->product->getMedia()->count() === 1) {
-            return $this->product->getMedia();
+        $images = $this->product->getMedia();
+        if ($images->count() === 1) {
+            return $images;
         }
 
-        $images = $this->product->getMedia();
         $coverImageId = $this->product->getCoverId();
         $coverImage = $images->get($coverImageId);
         $images->remove($coverImageId);
