35bb73d1436a093910354b1498b779c534683669
Huzaifa Mustafa <24492269+zaifastafa@users.noreply.github.com>
2020-07-13 23:42:23 +0500

    SW-459-rating-filter-issue

--- src/Findologic/Request/Handler/FilterHandler.php
+++ src/Findologic/Request/Handler/FilterHandler.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace FINDOLOGIC\FinSearch\Findologic\Request\Handler;
 
 use FINDOLOGIC\Api\Requests\SearchNavigation\SearchNavigationRequest;
+use FINDOLOGIC\FinSearch\Findologic\Response\Xml21\Filter\Filter;
+use FINDOLOGIC\FinSearch\Findologic\Response\Xml21\Filter\RatingFilter;
 use FINDOLOGIC\FinSearch\Struct\FiltersExtension;
 use Shopware\Core\Content\Product\Events\ProductListingCriteriaEvent;
 use Shopware\Core\Framework\Event\ShopwareEvent;
@@ -13,7 +15,6 @@ use Symfony\Component\HttpFoundation\Request;
 class FilterHandler
 {
     protected const FILTER_DELIMITER = '|';
-
     protected const MIN_PREFIX = 'min-';
     protected const MAX_PREFIX = 'max-';
 
@@ -31,7 +32,12 @@ class FilterHandler
         if ($selectedFilters) {
             foreach ($selectedFilters as $filterName => $filterValues) {
                 foreach ($this->getFilterValues($filterValues) as $filterValue) {
-                    $this->handleFilter($filterName, $filterValue, $searchNavigationRequest, $availableFilterNames);
+                    $this->handleFilter(
+                        $filterName,
+                        $filterValue,
+                        $searchNavigationRequest,
+                        $availableFilterNames
+                    );
                 }
             }
         }
@@ -41,7 +47,6 @@ class FilterHandler
      * Handles FINDOLOGIC-specific query params like "attrib" or "catFilter".
      * If any of these parameters are submitted, an URI may be returned that contains the query parameters
      * in a Shopware-readable format. If no FINDOLOGIC params are submitted, null may be returned.
-     *
      * E.g.
      * https://www.example.com/search?attrib%5Bvendor%5D%3DAdidas will return
      * https://www.example.com/search?manufacturer=Adidas
@@ -105,6 +110,13 @@ class FilterHandler
             return;
         }
 
+        if ($this->isRatingFilter($filterName)) {
+            $searchNavigationRequest->addAttribute($filterName, $filterValue, 'min');
+            $searchNavigationRequest->addAttribute($filterName, 5, 'max');
+
+            return;
+        }
+
         if (in_array($filterName, $availableFilterNames, true)) {
             $searchNavigationRequest->addAttribute($filterName, $filterValue);
         }
@@ -118,7 +130,7 @@ class FilterHandler
         $filterValue,
         SearchNavigationRequest $searchNavigationRequest
     ): void {
-        if (mb_substr($filterName, 0, mb_strlen(self::MIN_PREFIX)) === self::MIN_PREFIX) {
+        if (mb_strpos($filterName, self::MIN_PREFIX) === 0) {
             $filterName = mb_substr($filterName, mb_strlen(self::MIN_PREFIX));
             $searchNavigationRequest->addAttribute($filterName, $filterValue, 'min');
         } else {
@@ -167,11 +179,16 @@ class FilterHandler
 
     private function isMinRangeSlider(string $name): bool
     {
-        return mb_substr($name, 0, mb_strlen(self::MIN_PREFIX)) === self::MIN_PREFIX;
+        return mb_strpos($name, self::MIN_PREFIX) === 0;
     }
 
     private function isMaxRangeSlider(string $name): bool
     {
-        return mb_substr($name, 0, mb_strlen(self::MAX_PREFIX)) === self::MAX_PREFIX;
+        return mb_strpos($name, self::MAX_PREFIX) === 0;
+    }
+
+    private function isRatingFilter(string $filterName): bool
+    {
+        return $filterName === 'rating';
     }
 }
