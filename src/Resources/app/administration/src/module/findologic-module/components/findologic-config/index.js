2db5bc9517c15f7458f1035d33fabfe952f25fa6
Huzaifa Mustafa <24492269+zaifastafa@users.noreply.github.com>
2020-09-24 21:30:59 +0500

    Review changes

--- src/Resources/app/administration/src/module/findologic-module/components/findologic-config/index.js
+++ src/Resources/app/administration/src/module/findologic-module/components/findologic-config/index.js
@@ -1,7 +1,7 @@
-import template from './findologic-config.html.twig'
+import template from './findologic-config.html.twig';
 
-const { Component, Mixin } = Shopware
-const { Criteria, EntityCollection } = Shopware.Data
+const { Component, Mixin } = Shopware;
+const { Criteria } = Shopware.Data;
 
 Component.register('findologic-config', {
   template,
@@ -58,11 +58,11 @@ Component.register('findologic-config', {
       term: null,
       categories: [],
       categoryIds: []
-    }
+    };
   },
 
   created () {
-    this.createdComponent()
+    this.createdComponent();
   },
 
   methods: {
@@ -73,10 +73,10 @@ Component.register('findologic-config', {
      */
     checkTextFieldInheritance (value) {
       if (typeof value !== 'string') {
-        return true
+        return true;
       }
 
-      return value.length <= 0
+      return value.length <= 0;
     },
 
     /**
@@ -85,29 +85,33 @@ Component.register('findologic-config', {
      * @returns {boolean}
      */
     checkBoolFieldInheritance (value) {
-      return typeof value !== 'boolean'
+      return typeof value !== 'boolean';
     },
 
     /**
      * @public
+     * @param result
      * @param prop
      * @param order
      * @returns {function(*, *): number}
      */
-    compare (prop = 'name', order = 'asc') {
-      return function (a, b) {
-        // Use toUpperCase() to ignore character casing
-        const case1 = typeof a[prop] === 'string' ? a[prop].toUpperCase() : a[prop]
-        const case2 = typeof b[prop] === 'string' ? b[prop].toUpperCase() : b[prop]
+    sortByProperty (result, prop = 'name', order = 'asc') {
 
-        let comparison = 0
+      result.sort(function (a, b) {
+        // Use toUpperCase() to ignore character casing
+        const case1 = typeof a[prop] === 'string' ? a[prop].toUpperCase() : a[prop];
+        const case2 = typeof b[prop] === 'string' ? b[prop].toUpperCase() : b[prop];
+
+        let sort = 0;
         if (case1 > case2) {
-          comparison = order === 'asc' ? 1 : -1
+          sort = order === 'asc' ? 1 : -1;
         } else if (case1 < case2) {
-          comparison = order === 'asc' ? -1 : 1
+          sort = order === 'asc' ? -1 : 1;
         }
-        return comparison
-      }
+        return sort;
+      });
+
+      return result;
     },
 
     /**
@@ -115,18 +119,18 @@ Component.register('findologic-config', {
      */
     openSalesChannelUrl () {
       if (this.selectedSalesChannelId !== null) {
-        const criteria = new Criteria()
+        const criteria = new Criteria();
         criteria.addFilter(
           Criteria.equals('id', this.selectedSalesChannelId)
-        )
-        criteria.setLimit(1)
-        criteria.addAssociation('domains')
+        );
+        criteria.setLimit(1);
+        criteria.addAssociation('domains');
         this.salesChannelRepository.search(criteria, Shopware.Context.api).then((searchresult) => {
-          const domain = searchresult.first().domains.first()
-          this._openStagingUrl(domain)
-        })
+          const domain = searchresult.first().domains.first();
+          this._openStagingUrl(domain);
+        });
       } else {
-        this._openDefaultUrl()
+        this._openDefaultUrl();
       }
     },
 
@@ -134,8 +138,8 @@ Component.register('findologic-config', {
      * @private
      */
     _openDefaultUrl () {
-      const url = `${window.location.origin}?findologic=on`
-      window.open(url, '_blank')
+      const url = `${window.location.origin}?findologic=on`;
+      window.open(url, '_blank');
     },
 
     /**
@@ -144,40 +148,39 @@ Component.register('findologic-config', {
      */
     _openStagingUrl (domain) {
       if (domain) {
-        const url = `${domain.url}?findologic=on`
-        window.open(url, '_blank')
+        const url = `${domain.url}?findologic=on`;
+        window.open(url, '_blank');
       } else {
-        this._openDefaultUrl()
+        this._openDefaultUrl();
       }
     },
 
     createdComponent () {
-      this.getCategories()
+      this.getCategories();
     },
 
     /**
      * @public
      */
     getCategories () {
-      this.isLoading = true
+      this.isLoading = true;
 
-      const translatedCategories = []
-      this.categoryRepository.search(this.criteria, Shopware.Context.api).then((items) => {
-        this.term = null
-        this.total = items.total
+      const translatedCategories = [];
+      this.categoryRepository.search(this.categoryCriteria, Shopware.Context.api).then((items) => {
+        this.term = null;
+        this.total = items.total;
         items.forEach((category) => {
-
           translatedCategories.push({
             value: category.id,
             name: category.name,
             label: category.translated.breadcrumb.join(' > ')
-          })
-        })
+          });
+        });
 
-        this.categories = translatedCategories.sort(this.compare('label'))
+        this.categories = this.sortByProperty(translatedCategories, 'label');
       }).finally(() => {
-        this.isLoading = false
-      })
+        this.isLoading = false;
+      });
     }
   },
 
@@ -187,29 +190,27 @@ Component.register('findologic-config', {
      * @returns {boolean}
      */
     showTestButton () {
-      return this.isActive && this.shopkeyAvailable && this.isValidShopkey && this.isStagingShop
+      return this.isActive && this.shopkeyAvailable && this.isValidShopkey && this.isStagingShop;
     },
 
     salesChannelRepository () {
-      return this.repositoryFactory.create('sales_channel')
+      return this.repositoryFactory.create('sales_channel');
     },
 
     categoryRepository () {
-      return this.repositoryFactory.create('category')
-
+      return this.repositoryFactory.create('category');
     },
 
-    criteria () {
-      const criteria = new Criteria(1, 500)
-      criteria.addSorting(Criteria.sort('name', 'ASC'))
-      criteria.addSorting(Criteria.sort('parentId', 'ASC'))
+    categoryCriteria () {
+      const criteria = new Criteria(1, 500);
+      criteria.addSorting(Criteria.sort('name', 'ASC'));
+      criteria.addSorting(Criteria.sort('parentId', 'ASC'));
 
       if (this.term) {
-        criteria.addFilter(Criteria.contains('name', this.term))
+        criteria.addFilter(Criteria.contains('name', this.term));
       }
 
-      return criteria
+      return criteria;
     }
-
   }
-})
+});
