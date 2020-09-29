7a6959b0b94c3a79a3fde49faca0aa9e93a9029b
Tobias Graml <t.graml@findologic.com>
2020-09-29 18:12:06 +0200

    Update navigation category for test-databases

--- tests/Constants.php
+++ tests/Constants.php
@@ -6,5 +6,5 @@ namespace FINDOLOGIC\FinSearch\Tests;
 
 class Constants
 {
-    public const NAVIGATION_CATEGORY = 'b3bbb41154ac4dd793bbdd597d7dc63d';
+    public const NAVIGATION_CATEGORY = '2a9057fa48f9443da866556e359a3825';
 }
