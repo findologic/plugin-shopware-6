5400830c0adac9ab869f8aa8974821dd939edc18
Dominik Brader <d.brader@findologic.com>
2020-11-09 16:50:08 +0100

    Remove oudated comment

--- src/Export/Definitions/XmlFields.php
+++ src/Export/Definitions/XmlFields.php
@@ -6,9 +6,6 @@ namespace FINDOLOGIC\FinSearch\Export\Definitions;
 
 class XmlFields
 {
-    /**
-     * Products can contain these fields, if some are not set, the default will be used instead.
-     */
     public const FIELDS = [
         'name',
         'attributes',
