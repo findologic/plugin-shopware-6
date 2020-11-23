69abcbc68aec695cb29a3dba328b505683fbf190
Dominik Brader <d.brader@findologic.com>
2020-11-23 10:38:19 +0100

    Refactor services to own files

--- src/Storefront/Controller/SearchController.php
+++ src/Storefront/Controller/SearchController.php
@@ -10,12 +10,13 @@ use Shopware\Core\Framework\Routing\Annotation\RouteScope;
 use Shopware\Core\Framework\Routing\Exception\MissingRequestParameterException;
 use Shopware\Core\System\SalesChannel\SalesChannelContext;
 use Shopware\Storefront\Controller\SearchController as ShopwareSearchController;
+use Shopware\Storefront\Controller\StorefrontController;
 use Shopware\Storefront\Framework\Cache\Annotation\HttpCache;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class SearchController
+class SearchController extends StorefrontController
 {
     /** @var ShopwareSearchController */
     private $decorated;
@@ -26,8 +27,7 @@ class SearchController
     public function __construct(
         ShopwareSearchController $decorated,
         FilterHandler $filterHandler
-    )
-    {
+    ) {
         $this->decorated = $decorated;
         $this->filterHandler = $filterHandler;
     }
