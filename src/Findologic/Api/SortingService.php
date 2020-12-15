5fbb6768235cecdc7104f9b86a3dda1b53e5e573
Dominik Brader <d.brader@findologic.com>
2020-12-15 15:09:20 +0100

    PH-81_fix_sorting_for_newer_shopware_versions (SW-550) (#140)
    * Fixed a bug that caused the sorting option not to update properly for Shopware versions above 6.3.3.0.

---
+++ src/Findologic/Api/SortingService.php
@@ -0,0 +1,109 @@
+<?php
+
+declare(strict_types=1);
+
+namespace FINDOLOGIC\FinSearch\Findologic\Api;
+
+use FINDOLOGIC\FinSearch\Utils\Utils;
+use Shopware\Core\Content\Product\Events\ProductListingCriteriaEvent;
+use Shopware\Core\Content\Product\Events\ProductListingResultEvent;
+use Shopware\Core\Content\Product\SalesChannel\Listing\ProductListingSorting;
+use Shopware\Core\Content\Product\SalesChannel\Listing\ProductListingSortingRegistry;
+use Shopware\Core\Content\Product\SalesChannel\Sorting\ProductSortingCollection;
+use Shopware\Core\Content\Product\SalesChannel\Sorting\ProductSortingEntity;
+use Shopware\Core\Framework\Uuid\Uuid;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+class SortingService
+{
+    public const DEFAULT_SORT = 'score';
+
+    /** @var ProductListingSortingRegistry|null */
+    private $legacySortingRegistry;
+
+    /** @var TranslatorInterface */
+    private $translator;
+
+    public function __construct(
+        ?ProductListingSortingRegistry $legacySortingRegistry,
+        TranslatorInterface $translator
+    ) {
+        $this->legacySortingRegistry = $legacySortingRegistry;
+        $this->translator = $translator;
+    }
+
+    public function handleRequest(ProductListingCriteriaEvent $event): void
+    {
+        if (!Utils::versionLowerThan('6.3.3.0')) {
+            $this->addTopResultSorting($event);
+        }
+    }
+
+    public function handleResult(ProductListingResultEvent $event): void
+    {
+        if (Utils::versionLowerThan('6.3.3.0')) {
+            $this->addLegacyTopResultSorting($event);
+        }
+    }
+
+    protected function addTopResultSorting(ProductListingCriteriaEvent $event): void
+    {
+        /** @var ProductSortingCollection $availableSortings */
+        $availableSortings = $event->getCriteria()->getExtension('sortings') ?? new ProductSortingCollection();
+
+        $sortByScore = new ProductSortingEntity();
+        $sortByScore->setId(Uuid::randomHex());
+        $sortByScore->setActive(true);
+        $sortByScore->setTranslated(['label' => $this->translator->trans('filter.sortByScore')]);
+        $sortByScore->setKey('score');
+        $sortByScore->setPriority(5);
+        $sortByScore->setFields([
+            [
+                'field' => '_score',
+                'order' => 'desc',
+                'priority' => 1,
+                'naturalSorting' => 0,
+            ],
+        ]);
+
+        $availableSortings->add($sortByScore);
+
+        $event->getCriteria()->addExtension('sortings', $availableSortings);
+    }
+
+    protected function addLegacyTopResultSorting(ProductListingResultEvent $event): void
+    {
+        $currentSorting = $this->getCurrentLegacySorting($event->getRequest(), self::DEFAULT_SORT);
+
+        $event->getResult()->setSorting($currentSorting);
+        $this->legacySortingRegistry->add(
+            new ProductListingSorting('score', 'filter.sortByScore', ['_score' => 'desc'])
+        );
+        $sortings = $this->legacySortingRegistry->getSortings();
+        /** @var ProductListingSorting $sorting */
+        foreach ($sortings as $sorting) {
+            $sorting->setActive($sorting->getKey() === $currentSorting);
+        }
+
+        $event->getResult()->setSortings($sortings);
+    }
+
+    protected function getCurrentLegacySorting(Request $request, string $default): ?string
+    {
+        $key = $request->get('order', $default);
+        if (Utils::versionLowerThan('6.2')) {
+            $key = $request->get('sort', $default);
+        }
+
+        if (!$key) {
+            return null;
+        }
+
+        if ($this->legacySortingRegistry->has($key)) {
+            return $key;
+        }
+
+        return $default;
+    }
+}
