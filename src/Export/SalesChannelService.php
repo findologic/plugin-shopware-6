a240feb54462a326b125c83e19c2d3e187c62485
Dominik Brader <d.brader@findologic.com>
2020-12-16 16:05:42 +0100

    SW-543_fix_translated_non_seo_urls (#133)
    * Fixed a bug that caused products not to be exported, when their main variant has been inactive.

--- src/Export/SalesChannelService.php
+++ src/Export/SalesChannelService.php
@@ -8,9 +8,12 @@ use FINDOLOGIC\FinSearch\Findologic\Config\FinSearchConfigEntity;
 use Shopware\Core\Framework\DataAbstractionLayer\EntityRepository;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsFilter;
+use Shopware\Core\Framework\Routing\RequestTransformerInterface;
+use Shopware\Core\System\SalesChannel\Aggregate\SalesChannelDomain\SalesChannelDomainEntity;
 use Shopware\Core\System\SalesChannel\Context\SalesChannelContextFactory;
 use Shopware\Core\System\SalesChannel\Context\SalesChannelContextService;
 use Shopware\Core\System\SalesChannel\SalesChannelContext;
+use Symfony\Component\HttpFoundation\Request;
 
 class SalesChannelService
 {
@@ -20,12 +23,17 @@ class SalesChannelService
     /** @var SalesChannelContextFactory */
     private $salesChannelContextFactory;
 
+    /** @var RequestTransformerInterface */
+    private $requestTransformer;
+
     public function __construct(
         EntityRepository $systemConfigRepository,
-        SalesChannelContextFactory $salesChannelContextFactory
+        SalesChannelContextFactory $salesChannelContextFactory,
+        RequestTransformerInterface $requestTransformer
     ) {
         $this->systemConfigRepository = $systemConfigRepository;
         $this->salesChannelContextFactory = $salesChannelContextFactory;
+        $this->requestTransformer = $requestTransformer;
     }
 
     /**
@@ -53,5 +61,36 @@ class SalesChannelService
         }
 
         return null;
+    }
+
+    /**
+     * Builds a new Request instance based on the given SalesChannelContext. The new instance is built
+     * with the Shopware RequestTransformer, which is automatically called for all Controllers.
+     */
+    public function getRequest(Request $originalRequest, SalesChannelContext $salesChannelContext): Request
+    {
+        $languageId = $salesChannelContext->getSalesChannel()->getLanguageId();
+        $domain = $salesChannelContext->getSalesChannel()->getDomains()->filterByProperty(
+            'languageId',
+            $languageId
+        )->first();
+
+        $parsedUrl = parse_url($domain->getUrl());
+
+        // There is no Request::setUrl(), therefore we need to duplicate the current request object.
+        // @see https://github.com/symfony/symfony/issues/14575#issuecomment-102942494
+        $request = $originalRequest->duplicate(
+            null,
+            null,
+            null,
+            null,
+            null,
+            array_merge($originalRequest->server->all(), [
+                'REQUEST_URI' => $parsedUrl['path'] ?? '/',
+                'HTTP_HOST' => $parsedUrl['host'] . (isset($parsedUrl['port']) ? ':' . $parsedUrl['port'] : ''),
+            ])
+        );
+
+        return $this->requestTransformer->transform($request);
     }
 }
