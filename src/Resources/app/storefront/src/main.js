6c8a5c26d0a442e6920361ba80def68a7d878c47
Dominik Brader <d.brader@findologic.com>
2021-02-01 14:54:21 +0100

    Add basic Webpack integration with nouislider

--- src/Resources/app/storefront/src/main.js
+++ src/Resources/app/storefront/src/main.js
@@ -1,9 +1,26 @@
 /** Import JavaScript plugin classes */
 import FilterCategorySelect from './js/filter-category-select.plugin';
+import '../node_modules/nouislider/distribute/nouislider.css';
+
+const noUiSlider = require('@nouislider');
 
 /** Register plugins in the plugin manager */
 const PluginManager = window.PluginManager;
 PluginManager.register('FilterCategorySelect', FilterCategorySelect, '[data-filter-category-select]');
+
+// This code may be removed, but left there as an example. Once uncommented, it will create a range-slider
+// above the page header.
+// const slider = document.createElement('div');
+// document.body.prepend(slider);
+//
+// noUiSlider.create(slider, {
+//     start: [20, 80],
+//     connect: true,
+//     range: {
+//         'min': 0,
+//         'max': 100
+//     }
+// });
 
 if (module.hot) {
     module.hot.accept();
