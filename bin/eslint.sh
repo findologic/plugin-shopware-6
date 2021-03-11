8e71550dee9ecec921d136349d17cbd3fcaa0f76
Huzaifa Mustafa <24492269+zaifastafa@users.noreply.github.com>
2021-03-11 22:53:18 +0500

    Review changes

--- bin/eslint.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-../../../vendor/shopware/platform/src/Administration/Resources/app/administration/node_modules/.bin/eslint --ignore-path .eslintignore --config ../../../vendor/shopware/platform/src/Administration/Resources/app/administration/.eslintrc.js --ext .js,.vue src/Resources/app/administration
-../../../vendor/shopware/platform/src/Administration/Resources/app/administration/node_modules/.bin/eslint --ignore-path .eslintignore --config ../../../vendor/shopware/platform/src/Administration/Resources/app/administration/.eslintrc.js --ext .js,.vue src/Resources/app/storefront
