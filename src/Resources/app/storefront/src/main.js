1da40a741e1d65ad93cbdca253ac8938fc29794f
Dominik Brader <d.brader@findologic.com>
2021-04-26 14:50:04 +0200

    Revert "Revert "SW-504 support filter type slider (#169)""
    This reverts commit 60764c954da80ec26c8de6dda90266170c9f3f9d.

--- src/Resources/app/storefront/src/main.js
+++ src/Resources/app/storefront/src/main.js
@@ -1,11 +1,13 @@
 /** Import JavaScript plugin classes */
 import FilterCategorySelect from './js/filter-category-select.plugin';
 import FilterPropertySelect from './js/filter-property-select.plugin';
+import FilterSliderRange from './js/filter-slider-range.plugin';
 
 /** Register plugins in the plugin manager */
 const PluginManager = window.PluginManager;
 PluginManager.register('FilterCategorySelect', FilterCategorySelect, '[data-filter-category-select]');
 PluginManager.override('FilterPropertySelect', FilterPropertySelect, '[data-filter-property-select]');
+PluginManager.register('FilterSliderRange', FilterSliderRange, '[data-filter-slider-range]');
 
 if (module.hot) {
     module.hot.accept();
