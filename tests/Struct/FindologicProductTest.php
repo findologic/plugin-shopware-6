746d3571d0631170a9f5af3d72a654d8ac6474d8
Dominik Brader <d.brader@findologic.com>
2021-08-03 11:09:59 +0200

    Fix unrelated test

--- tests/Struct/FindologicProductTest.php
+++ tests/Struct/FindologicProductTest.php
@@ -1381,6 +1381,12 @@ class FindologicProductTest extends TestCase
      */
     public function testProductListPrice(?string $currencyId, bool $isPriceAvailable): void
     {
+        if ($currencyId === null && !Utils::versionLowerThan('6.4.2.0')) {
+            $this->markTestSkipped(
+                'SW >= 6.4.2.0 requires a price to be set for the default currency. Therefore not testable.'
+            );
+        }
+
         if ($currencyId === null) {
             $currencyId = $this->createCurrency();
         }
