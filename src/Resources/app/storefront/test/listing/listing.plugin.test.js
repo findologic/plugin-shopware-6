5275e2e441bf63eeef49a2c87e306d233c3a6e94
Dominik Brader <d.brader@findologic.com>
2022-01-24 16:56:13 +0100

    Add implementation and unit-test

---
+++ src/Resources/app/storefront/test/listing/listing.plugin.test.js
@@ -0,0 +1,90 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import FlListingPlugin from "../../src/js/listing/listing.plugin";
+
+describe('listing.plugin,js', () => {
+  let listingPlugin = undefined;
+  let spyInit = jest.fn();
+  let spyInitializePlugins = jest.fn();
+
+  beforeEach(() => {
+    // create mocks
+    window.csrf = {
+      enabled: false
+    };
+
+    window.router = [];
+
+    window.PluginManager = {
+      getPluginInstancesFromElement: () => {
+        return new Map();
+      },
+      getPlugin: () => {
+        return {
+          get: () => []
+        };
+      },
+      initializePlugins: undefined
+    };
+
+    // mock listing plugins
+    const mockElement = document.createElement('div');
+    listingPlugin = new FlListingPlugin(mockElement);
+    listingPlugin._registry = [];
+
+    // create spy elements
+    listingPlugin.init = spyInit;
+    window.PluginManager.initializePlugins = spyInitializePlugins;
+  });
+
+  afterEach(() => {
+    listingPlugin = undefined;
+    spyInit.mockClear();
+    spyInitializePlugins.mockClear();
+    window.PluginManager.initializePlugins = undefined;
+  });
+
+  test('listing plugin exists', () => {
+    expect(typeof listingPlugin).toBe('object');
+  });
+
+  test.each([
+    '#navigation:search=&attrib%5Bcat_url%5D%5B0%5D=%2FKids%2F',
+    '#search:search=blub&query=blub',
+    '#suggest:suggest'
+  ])('history must not be changed on Direct Integration page %s', (hash) => {
+    const mockElement = document.createElement('div');
+    const cmsElementProductListingWrapper = document.createElement('div');
+    cmsElementProductListingWrapper.classList.add('cms-element-product-listing-wrapper');
+
+    document.body.append(cmsElementProductListingWrapper);
+
+    listingPlugin = new FlListingPlugin(mockElement);
+
+    jest.spyOn(listingPlugin, 'refreshRegistry');
+    window.location.hash = hash;
+
+    listingPlugin._onWindowPopstate();
+
+    expect(listingPlugin.refreshRegistry).not.toHaveBeenCalled();
+  });
+
+  test('history must be changed on regular non-Direct Integration pages', () => {
+    const mockElement = document.createElement('div');
+    const cmsElementProductListingWrapper = document.createElement('div');
+    cmsElementProductListingWrapper.classList.add('cms-element-product-listing-wrapper');
+
+    document.body.append(cmsElementProductListingWrapper);
+
+    listingPlugin = new FlListingPlugin(mockElement);
+
+    jest.spyOn(listingPlugin, 'refreshRegistry');
+    window.location.hash = '#shopware';
+
+    listingPlugin._onWindowPopstate();
+
+    expect(listingPlugin.refreshRegistry).toHaveBeenCalled();
+  });
+});
