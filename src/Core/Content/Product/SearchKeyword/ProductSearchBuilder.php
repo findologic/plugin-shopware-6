4da4383f6ab779fe9530ff0588af089b8a97b4e7
Daniel Blaichinger <d.blaichinger@findologic.com>
2022-02-18 09:04:57 +0100

    Check for Shopware version to use appropriate ProdcutSearchBuilder::build method

--- src/Core/Content/Product/SearchKeyword/ProductSearchBuilder.php
+++ src/Core/Content/Product/SearchKeyword/ProductSearchBuilder.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace FINDOLOGIC\FinSearch\Core\Content\Product\SearchKeyword;
 
+use FINDOLOGIC\FinSearch\Utils\Utils;
 use Shopware\Core\Content\Product\SearchKeyword\ProductSearchBuilder as ShopwareProductSearchBuilder;
 use Shopware\Core\Content\Product\SearchKeyword\ProductSearchTermInterpreterInterface;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
@@ -30,6 +31,64 @@ class ProductSearchBuilder extends ShopwareProductSearchBuilder
     }
 
     public function build(Request $request, Criteria $criteria, SalesChannelContext $context): void
+    {
+        if (Utils::versionLowerThan('6.4.0.0')) {
+            $this->buildShopware63AndLower($request, $criteria, $context);
+        } else {
+            $this->buildShopware64AndGreater($request, $criteria, $context);
+        }
+    }
+
+    public function buildParent(Request $request, Criteria $criteria, SalesChannelContext $context): void
+    {
+        parent::build($request, $criteria, $context);
+    }
+
+    private function buildShopware63AndLower(Request $request, Criteria $criteria, SalesChannelContext $context): void
+    {
+        if ($request->getPathInfo() === '/suggest') {
+            $this->buildParent($request, $criteria, $context);
+            return;
+        }
+
+        $search = $request->query->get('search');
+
+        if (is_array($search)) {
+            $term = implode(' ', $search);
+        } else {
+            $term = (string) $search;
+        }
+
+        $term = trim($term);
+
+        $pattern = $this->interpreter->interpret($term, $context->getContext());
+
+        foreach ($pattern->getTerms() as $searchTerm) {
+            $criteria->addQuery(
+                new ScoreQuery(
+                    new EqualsFilter('product.searchKeywords.keyword', $searchTerm->getTerm()),
+                    $searchTerm->getScore(),
+                    'product.searchKeywords.ranking'
+                )
+            );
+        }
+        $criteria->addQuery(
+            new ScoreQuery(
+                new ContainsFilter('product.searchKeywords.keyword', $pattern->getOriginal()->getTerm()),
+                $pattern->getOriginal()->getScore(),
+                'product.searchKeywords.ranking'
+            )
+        );
+
+        $criteria->addFilter(
+            new EqualsAnyFilter('product.searchKeywords.keyword', array_values($pattern->getAllTerms()))
+        );
+        $criteria->addFilter(
+            new EqualsFilter('product.searchKeywords.languageId', $context->getContext()->getLanguageId())
+        );
+    }
+
+    private function buildShopware64AndGreater(Request $request, Criteria $criteria, SalesChannelContext $context): void
     {
         if ($request->getPathInfo() === '/suggest') {
             $this->buildParent($request, $criteria, $context);
@@ -79,10 +138,5 @@ class ProductSearchBuilder extends ShopwareProductSearchBuilder
                 new EqualsAnyFilter('product.searchKeywords.keyword', $terms),
             ]));
         }
-    }
-
-    public function buildParent(Request $request, Criteria $criteria, SalesChannelContext $context): void
-    {
-        parent::build($request, $criteria, $context);
     }
 }
