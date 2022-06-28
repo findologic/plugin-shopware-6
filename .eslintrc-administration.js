a48fce3960aaee10f059eb60a4894005dd255528
Tobias Graml <t.graml@findologic.com>
2022-06-28 14:51:06 +0200

    Use correct linting for administration and storefront

---
+++ .eslintrc-administration.js
@@ -0,0 +1,6 @@
+module.exports = {
+    extends: [
+        '../../../vendor/shopware/platform/src/Administration/Resources/app/administration/.eslintrc.js',
+        '.eslintrc.js',
+    ],
+};
---
+++ .eslintrc-storefront.js
@@ -0,0 +1,9 @@
+module.exports = {
+    env: {
+        jest: true
+    },
+    extends: [
+        '../../../vendor/shopware/platform/src/Storefront/Resources/app/storefront/.eslintrc.js',
+        '.eslintrc.js',
+    ],
+};
