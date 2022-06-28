07c44a4096cabd794d83329e465665e32cb98dfb
Tobias Graml <t.graml@findologic.com>
2022-06-28 14:56:59 +0200

    Set noUiSlider global

--- .eslintrc-storefront.js
+++ .eslintrc-storefront.js
@@ -2,6 +2,9 @@ module.exports = {
     env: {
         jest: true
     },
+    globals: {
+        noUiSlider: true
+    },
     extends: [
         '../../../vendor/shopware/platform/src/Storefront/Resources/app/storefront/.eslintrc.js',
         '.eslintrc.js',
