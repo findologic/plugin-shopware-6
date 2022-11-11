eb3165496fc0228736c7e61ba0555f08746762d3
Tobias Graml <t.graml@findologic.com>
2022-11-11 10:09:45 +0100

    Fix linting

--- src/Export/Search/CategorySearcher.php
+++ src/Export/Search/CategorySearcher.php
@@ -1,12 +1,13 @@
 <?php
 
+declare(strict_types=1);
+
 namespace FINDOLOGIC\FinSearch\Export\Search;
 
 use FINDOLOGIC\FinSearch\Utils\Utils;
 use FINDOLOGIC\Shopware6Common\Export\ExportContext;
 use FINDOLOGIC\Shopware6Common\Export\Search\AbstractCategorySearcher;
 use Shopware\Core\Content\Category\CategoryCollection;
-use Shopware\Core\Framework\Context;
 use Shopware\Core\Framework\DataAbstractionLayer\EntityRepository;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\ContainsFilter;
