7558c7c82ddc26f3d4ba9b09f09e6782ff88f8e5
Bojan <bojan.dimitrijevic@soprex.com>
2023-11-09 16:39:41 +0100

    Small refactor for the client logic

--- src/Findologic/Client/ServiceConfigClient.php
+++ src/Findologic/Client/ServiceConfigClient.php
@@ -15,11 +15,20 @@ class ServiceConfigClient
         private readonly string $shopkey,
         private ?Client $client = null
     ) {
-        if (!isset($this->client)) {
-            $this->client = new Client(['base_uri' => BaseUrl::CDN]);
-        } elseif ($this->client->getConfig()['base_uri'] != BaseUrl::CDN) {
+        $this->initializeClient();
+    }
+
+    private function initializeClient(): void
+    {
+        if (!$this->client || $this->isBaseUriDifferent()) {
             $this->client = new Client(['base_uri' => BaseUrl::CDN]);
         }
+    }
+
+    private function isBaseUriDifferent(): bool
+    {
+        $currentBaseUri = $this->client->getConfig()['base_uri'] ?? null;
+        return $currentBaseUri !== BaseUrl::CDN;
     }
 
     /**
