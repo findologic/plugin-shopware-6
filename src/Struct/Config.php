eece751831ce0e5ff839ebd432347d8ea42d67a2
Bojan <bojan.dimitrijevic@soprex.com>
2024-09-02 08:54:26 +0200

    Refactor Config method

--- src/Struct/Config.php
+++ src/Struct/Config.php
@@ -156,6 +156,8 @@ class Config extends Struct
         $salesChannelId = $salesChannel->getId();
         $languageId = $salesChannelContext->getLanguageId();
 
+        var_dump($languageId , $salesChannel->getLanguageId() );
+
         $this->active = $this->getConfig($salesChannelId, $languageId, 'FinSearch.config.active', false);
         $this->shopkey = $this->getConfig($salesChannelId, $languageId, 'FinSearch.config.shopkey');
         $this->activeOnCategoryPages = $this->getConfig(
