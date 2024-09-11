4bf55139dd112345df67b71b95261337e88443e4
Bojan <bojan.dimitrijevic@soprex.com>
2024-09-11 14:07:04 +0200

    Remove var dump

--- src/Struct/Config.php
+++ src/Struct/Config.php
@@ -156,8 +156,6 @@ class Config extends Struct
         $salesChannelId = $salesChannel->getId();
         $languageId = $salesChannelContext->getLanguageId();
 
-        var_dump($languageId , $salesChannel->getLanguageId());
-
         $this->active = $this->getConfig($salesChannelId, $languageId, 'FinSearch.config.active', false);
         $this->shopkey = $this->getConfig($salesChannelId, $languageId, 'FinSearch.config.shopkey');
         $this->activeOnCategoryPages = $this->getConfig(
