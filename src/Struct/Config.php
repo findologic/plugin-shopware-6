85d88716b9fa5b585459569640ad0ad1010ba7ed
Radomir Butacevic <radomir.butacevic@soprex.com>
2024-09-18 15:12:54 +0200

    Correct the changes

--- src/Struct/Config.php
+++ src/Struct/Config.php
@@ -152,8 +152,11 @@ class Config extends Struct
      */
     public function initializeBySalesChannel(SalesChannelContext $salesChannelContext): void
     {
-        $salesChannelId = $salesChannelContext->getSalesChannel()->getId();
-        $languageId = $salesChannelContext->getLanguageId();
+        $salesChannel = $salesChannelContext->getSalesChannel();
+        $salesChannelId = $salesChannel->getId();
+        if (!$languageId = $salesChannelContext->getLanguageId()) {
+            $languageId = $salesChannel->getId();
+        }
 
         $this->active = $this->getConfig($salesChannelId, $languageId, 'FinSearch.config.active', false);
         $this->shopkey = $this->getConfig($salesChannelId, $languageId, 'FinSearch.config.shopkey');
